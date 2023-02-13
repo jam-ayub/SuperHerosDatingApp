e1f910975a7ad341cc5b786553d023616cfeab7b
Jam <jamayub098@gmail.com>
2023-02-13 22:55:14 +0500

    Added Routing in Angular Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/User';
 import { AccountService } from '../_services/account.service';
@@ -11,36 +13,23 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  //loggedIn: boolean;
-  //currentUser$: Observable<User>;
 
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    //this.getCurrentUser();
-    //this.currentUser$ = this.accountService.currentUser$;
   }
 
   login(){
     this.accountService.login(this.model).subscribe(responce => {
-      console.log(responce);
-      //this.loggedIn = true;
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logOut() {
     this.accountService.logout();
-    //this.loggedIn = false;
+    this.router.navigateByUrl('/');
   }
-
-  // getCurrentUser(){
-  //   this.accountService.currentUser$.subscribe(user => {
-  //     this.loggedIn = !!user;
-  //   }, error => {
-  //     console.log(error);
-  //   });
-  // }
-
 }
